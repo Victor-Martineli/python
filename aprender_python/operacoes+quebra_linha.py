e09ab1bdf66205fba4dea505bb9f6343f82c1ac9
Victor Martineli <victor.martineli999@gmail.com>
2024-03-11 22:10:10 -0300

    fist commit

---
+++ aprender_python/operacoes+quebra_linha.py
@@ -0,0 +1,38 @@
+# \n é usado como quebra de linha
+print('qualquer coisa \n coisa qualquer')
+
+#operações aritiméticas
+#soma
+soma = 5 + 5
+print(soma)
+
+#subtração
+subtracao = 7 - 5
+print(subtracao)
+
+#multiplicação
+mutiplicacao = 2 * 2
+print(mutiplicacao)
+
+#divisão
+divisao = 10 / 2
+print(divisao)
+
+#divisão inteira
+divisao_inteira = 9 // 3
+print(divisao_inteira)
+
+#módulo
+resto = 9 % 3
+print(resto)
+
+#potência
+potenciacao = 8 ** 3
+print(potenciacao)
+
+
+# + e * também pode ser usado com string
+texto1 = 'hello'
+texto2 = ' world'
+print (texto1 + texto2)
+print (texto1 * 3)

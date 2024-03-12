d0b7cbedffa55d3c70f5b834584973104d1ffc14
Victor Martineli <115561211+Victor-Martineli@users.noreply.github.com>
2024-03-11 21:53:07 -0300

    Add files via upload

---
+++ aprender_python/advinhacao_final.py
@@ -0,0 +1,50 @@
+print('******************************')
+print('*    Jogo da adivinhação     *')
+print('******************************')
+
+numero_secreto = 42
+pontuacao_inicial = 1000
+nivel = int(input('selecione entre o nivel 1, 2 ou 3: '))
+if nivel == 1:
+    tentativas = 20
+    penalidade = 50
+
+elif nivel == 2:
+    tentativas = 10
+    penalidade = 100
+
+elif nivel == 3:
+    tentativas = 5
+    penalidade = 200
+
+print('Você selecionou o nivel {}, possui {} tentativas e {} pontos'.format(nivel, tentativas, pontuacao_inicial))
+
+
+
+
+pontos_restantes = pontuacao_inicial - penalidade
+
+#for também pode ser usado para fazer loops
+for rodada in range(1,tentativas + 1):
+    print('Tentativa {} de {}'.format(rodada, tentativas))
+
+    chute = int(input('Advinhe o número: '))
+    print('Você digitou: ', chute)
+
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print('Você acertou!')
+        break
+
+    elif (maior):
+        print('Você errou! O seu chute foi maior que o número secreto, pontos restantes: {}'.format(pontos_restantes))
+    elif(menor):
+        print('Você errou! o seu chute foi menor que o número secreto, pontos restantes: {}'.format(pontos_restantes))
+    pontos_restantes = pontos_restantes - penalidade
+
+
+
+print('Fim do jogo')

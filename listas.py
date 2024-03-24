f234660199291798f0e1930a0cd5dcd285af46b8
Victor Pierobon <victor.martineli999@gmail.com>
2024-03-24 17:07:27 -0300

    lista

--- listas.py
+++ listas.py
@@ -1,6 +1,6 @@
 
 # listas podem ser compostas de int, float e string
-lista = [1,2,3,4]
+lista = [1, 2, 3, 4]
 # se quisemos que apenas um elemento específico de uma lista seja exibido
 # basta utilizar o seguinte comando
 print(lista[2])
@@ -13,3 +13,34 @@ while(n < 4):
     if 1 <= mes < 13:
         print('o mês é {} '.format(meses[mes-1]))
     n += 1
+
+lista2 = [2, 3, 5, 7, 11]
+# outras operações que podem ser feitas com listas são as seguintes
+
+print(lista2[0:2])
+# ou print(lista2[:2]) que vamos pegar até o item 2 da lista
+
+print(lista2[2:])
+# vai pegar do item 2 em diante até o fim da lista
+
+print(lista2[-3:])
+# colocando o - invertemos a ordem, pega do final para o começo
+
+print(lista2[2:4])
+# pega um recorte da lista
+
+lista3 = []
+
+lista3.append('zero')
+print(lista3)
+# a função append() adiciona um elemento por vez na lista
+# para inserir mais elementos pode-se somar ou multiplicar listas
+# outra opção também é usar a função extend()
+lista3.extend(['dois', 'tres'])
+print(lista3)
+
+lista3 += ['um']
+print(lista3)
+
+lista3 *= 2
+print(lista3)

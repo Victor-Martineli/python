88c1cfad6d12e6837579f3354c2528334beb1a86
Victor Pierobon <victor.martineli999@gmail.com>
2024-03-31 17:45:08 -0300

    problemas nos exercicios

--- workspace/conta.py
+++ workspace/conta.py
@@ -36,6 +36,9 @@ class Conta:
         self._limite = limite
         self.historico = Historico()
 
+    def __str__(self):
+        return 'dados da conta: \nnumero: {} \ntitular: {} \nsaldo: {} \nlimite: {}'.format(self._numero, self._titular, self._saldo, self._limite)
+
 # quando um elemento esta com um _ antes de seu nome indica que ele não deveria ser alterado de forma direta, porem como
 # no python não temos uma classe private isso ainda é possivel porem poderia ser desastroso, então seguimos a convenção
 # de não usa-los diretamente, então para modifica-los, usamos os getters e setters para atribuir e mudar seus valores.
@@ -82,5 +85,61 @@ class Conta:
             self.historico.transacoes.append('transferência de {} para conta {}'.format(valor, destino.numero))
             return True
 
+    def atualiza(self, taxa):
+        self._saldo += self._saldo * taxa
+
 # quando criamos um novo objeto não precisamos do metodo __new__ pois o proprio python o está executando por baixo dos
 # panos, logo em seguida usa o metodo __init__ ptoda vez que criamos uma conta nova
+
+
+class ContaCorrente(Conta):
+
+    def __init__(self, numero, cliente, saldo, limite=1000.0):
+        super().__init__(numero, cliente, saldo, limite)
+
+    def atualiza(self, taxa):
+        self._saldo += self._saldo * 2
+
+    def deposita(self, valor):
+        self._saldo += valor - 0.10
+
+
+class ContaPoupanca(Conta):
+    def __init__(self, numero, cliente, saldo, limite=1000.0):
+        super().__init__(numero, cliente, saldo, limite)
+
+    def atualiza(self, taxa):
+        self._saldo += self._saldo * 3
+
+
+class AtualizadorDeContas:
+    def __init__(self, selic, saldo_total=0):
+        self._selic = selic
+        self._saldo_total = saldo_total
+
+    def roda(self, conta):
+        print('saldo da conta: {}'.format(conta.saldo))
+        self._saldo_total += conta.atualiza(self._selic)
+        print('Saldo Final: {}'.format(self._saldo_total))
+
+
+if __name__ == '__main__':
+    c = Conta('123-4', 'victor', 1000.0)
+    cc = ContaCorrente('123-5', 'rafael', 1000.0)
+    cp = ContaPoupanca('123-6', 'caroline', 1000.0)
+
+    # c.atualiza(0.01)
+    # cc.atualiza(0.01)
+    # cp.atualiza(0.01)
+
+    # print(c.extrato())
+    # print(cc.extrato())
+    # print(cp.extrato())
+    # print(cc)
+    adc = AtualizadorDeContas(0.01)
+
+    adc.roda(c)
+    adc.roda(cc)
+    adc.roda(cp)
+
+    print('saldo total: {}'.format(adc.saldo_total))
